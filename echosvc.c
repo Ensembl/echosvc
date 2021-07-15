08ff6935d055fb4007a706f9804e4aa476d030ae
Dan Sheppard <dan@ebi.ac.uk>
2021-07-15 13:07:19 +0100

    Configurable port number.

--- echosvc.c
+++ echosvc.c
@@ -85,17 +85,35 @@ static void handle(int lfd,int cfd) {
   if(pid>0) { close(cfd); return; /* parent */ }
   close(lfd);
   process(cfd);
+  shutdown(cfd,2);
   close(cfd);
   exit(0);
 }
 
-int main() {
+static int get_int_or_die(char *value) {
+  int out;
+  char *end;
+
+  out = strtol(value,&end,10);
+  if(*value && !*end) {
+    return out;
+  } else {
+    die("bad port number");
+  }
+}
+
+int main(int argc, char **argv) {
   int lfd,cfd,i,r,on=1;
   struct sockaddr_in addr;
   struct sigaction sa;
+  int port = 7;
+  char *end;
 
-  daemonize();
-  report("echosvc started");
+  /* exctract port number */
+  if(argc>1) {
+    port = get_int_or_die(argv[1]);
+  }
+  /* startup */
   lfd = socket(AF_INET,SOCK_STREAM,0);
   if(lfd==-1) { die("Socket failed"); }
   /* Reusable */
@@ -111,11 +129,13 @@ int main() {
   memset(&addr,0,sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  addr.sin_port = htons(7);
+  addr.sin_port = htons(port);
   if(bind(lfd,(struct sockaddr *)&addr,sizeof(struct sockaddr))) {
     die("Bind failed");
   }
   if(listen(lfd,16)) { die("Listen failed"); }
+  report("echosvc started");
+  daemonize();
   while(1) {
     cfd = accept(lfd,0,0);
     if(kids>MAXCONN) {
